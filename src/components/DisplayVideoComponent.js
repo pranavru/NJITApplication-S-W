9c011194a33a7c277947d5149f4927850aed261a
Pranav Ruparelia <pranavruparelia9@gmail.com>
2020-06-12 15:19:29 -0400

    Video is updated

--- src/components/DisplayVideoComponent.js
+++ src/components/DisplayVideoComponent.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Animated } from 'react-animated-css';
-import { Card, UncontrolledCarousel } from 'reactstrap';
+import { Card } from 'reactstrap';
 import ReactPlayer from 'react-player'
 
 const DisplayVideoComponent = (props) => {
-    const items = props.videoSrc
+
+    const [playVideo, setToPlay] = React.useState(false);
+    // const [playVideo, setToPlay] = React.useState(false);
+    const url = props.videoSrc
     return (
         <div style={{ marginTop: '5%' }}>
-            {items !== "" ?
+            {url !== "" ?
                 <Animated
                     animationIn='fadeInUp' animationOut='fadeOut'
                     animationInDuration={400} animationOutDuration={600}
@@ -15,7 +18,11 @@ const DisplayVideoComponent = (props) => {
                 >
                     <Card style={{ padding: 4 }}>
                         {/* <UncontrolledCarousel items={items} controls={false} interval={500} slide={false} /> */}
-                        <ReactPlayer url={"http://18.191.247.248" + items} autoPlay={true} controls={true} width="26.7vw" height="30vh" style={{ marginLeft: 2 }} muted={true} playing={false} />
+                        <ReactPlayer
+                            url={"http://18.191.247.248" + url} controls={playVideo} width="26.7vw"
+                            height="30vh" style={{ marginLeft: 2 }} playing={playVideo} autoPlay muted
+                            onReady={() => setToPlay(true)} onStart={() => console.log("Video is playing? ", playVideo)}
+                            onPause={() => console.log("Video is playing? ", playVideo)} />
                     </Card>
                 </Animated> : <div className="loader"></div>}
         </div>
