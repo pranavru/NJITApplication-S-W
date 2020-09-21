7f8cddeaa534d419360d4d497d82bf8c95e91aa0
Pranav Ruparelia <pranavruparelia9@gmail.com>
2020-09-21 11:32:01 -0400

    basic changes

--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import {
-    Button, CardText, Spinner
-} from 'reactstrap'
+import { Spinner } from 'reactstrap'
 
 import { Animated } from 'react-animated-css';
 import LoadingOverlay from 'react-loading-overlay';
